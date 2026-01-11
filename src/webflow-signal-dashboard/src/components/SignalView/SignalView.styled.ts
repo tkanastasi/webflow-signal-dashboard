3146453038c0f6a63f96300019cc24f2680f5d9d
tkanastasi <159182171+tkanastasi@users.noreply.github.com>
2026-01-12 02:19:58 +0400

    SignalView responsive

--- src/webflow-signal-dashboard/src/components/SignalView/SignalView.styled.ts
+++ src/webflow-signal-dashboard/src/components/SignalView/SignalView.styled.ts
@@ -12,19 +12,19 @@ export const SignalViewContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(1, minmax(0px, 1fr));
 
-  @media (min-width: 900px) {
+  @media (min-width: 600px) {
     grid-template-columns: repeat(2, minmax(0, 1fr));
   }
   
-  @media (min-width: 1200px) {
+  @media (min-width: 900px) {
     grid-template-columns: repeat(3, minmax(0, 1fr));
   }
   
-  @media (min-width: 1500px) {
+  @media (min-width: 1200px) {
     grid-template-columns: repeat(4, minmax(0, 1fr));
   }
   
-  @media (min-width: 1800px) {
+  @media (min-width: 1500px) {
     grid-template-columns: repeat(5, minmax(0, 1fr));
   }
 `;
