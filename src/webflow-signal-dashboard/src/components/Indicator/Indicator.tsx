81e8eab82582e0feace356dca52f0700c9b43027
tkanastasi <159182171+tkanastasi@users.noreply.github.com>
2026-01-12 17:25:05 +0400

    Update Indicator.tsx

--- src/webflow-signal-dashboard/src/components/Indicator/Indicator.tsx
+++ src/webflow-signal-dashboard/src/components/Indicator/Indicator.tsx
@@ -9,6 +9,7 @@ import {
 } from "./Indicator.styled";
 import { PulsingDot } from "../SignalCard/SignalCard.styled";
 import { SparklineIcon } from "../../assets/svg";
+import { StyledComponentsShadowDomProvider } from "@webflow/styled-components-utils";
 
 export enum IndicatorType {
     MonthlyGrowth = "MonthlyGrowth",
@@ -44,53 +45,59 @@ const Indicator: React.FC<IndicatorProps> = (props) => {
 
     if (type === IndicatorType.MonthlyGrowth) {
         return (
-            <IndicatorContainer>
-                <IndicatorMain>
-                    <PulsingDot />
-                    <IndicatorValue $positive={value !== null && value >= 0}>
-                        {value !== null
-                            ? `${value >= 0 ? "+" : ""}${value.toFixed(1)}%`
-                            : "N/A"}
-                    </IndicatorValue>
-                    <SparklineWrapper>
-                        <SparklineIcon />
-                    </SparklineWrapper>
-                </IndicatorMain>
+            <StyledComponentsShadowDomProvider>
+                <IndicatorContainer>
+                    <IndicatorMain>
+                        <PulsingDot />
+                        <IndicatorValue $positive={value !== null && value >= 0}>
+                            {value !== null
+                                ? `${value >= 0 ? "+" : ""}${value.toFixed(1)}%`
+                                : "N/A"}
+                        </IndicatorValue>
+                        <SparklineWrapper>
+                            <SparklineIcon />
+                        </SparklineWrapper>
+                    </IndicatorMain>
 
-                <IndicatorSubtext>
-                    +3.4R Generated This Month
-                </IndicatorSubtext>
-            </IndicatorContainer>
+                    <IndicatorSubtext>
+                        +3.4R Generated This Month
+                    </IndicatorSubtext>
+                </IndicatorContainer>
+            </StyledComponentsShadowDomProvider>
         );
     }
     else if (type === IndicatorType.ProfitFactor) {
         return (
-            <IndicatorContainer>
-                <IndicatorMain>
-                    <IndicatorValue>
-                        {value !== null ? value.toFixed(1) : "N/A"}
-                    </IndicatorValue>
-                </IndicatorMain>
+            <StyledComponentsShadowDomProvider>
+                <IndicatorContainer>
+                    <IndicatorMain>
+                        <IndicatorValue>
+                            {value !== null ? value.toFixed(1) : "N/A"}
+                        </IndicatorValue>
+                    </IndicatorMain>
 
-                <IndicatorSubtext>
-                    For every $1 loss, we make ${value !== null ? value.toFixed(1) : "N/A"}
-                </IndicatorSubtext>
-            </IndicatorContainer>
+                    <IndicatorSubtext>
+                        For every $1 loss, we make ${value !== null ? value.toFixed(1) : "N/A"}
+                    </IndicatorSubtext>
+                </IndicatorContainer>
+            </StyledComponentsShadowDomProvider>
         );
     }
     else if (type === IndicatorType.WinRate) {
         return (
-            <IndicatorContainer>
-                <IndicatorMain>
-                    <IndicatorValue>
-                        {value !== null ? `${value.toFixed(1)}%` : "N/A"}
-                    </IndicatorValue>
-                </IndicatorMain>
+            <StyledComponentsShadowDomProvider>
+                <IndicatorContainer>
+                    <IndicatorMain>
+                        <IndicatorValue>
+                            {value !== null ? `${value.toFixed(1)}%` : "N/A"}
+                        </IndicatorValue>
+                    </IndicatorMain>
 
-                <IndicatorSubtext>
-                    (1,735 wins, 1,112 losses out of 2,847 total trades)
-                </IndicatorSubtext>
-            </IndicatorContainer>
+                    <IndicatorSubtext>
+                        (1,735 wins, 1,112 losses out of 2,847 total trades)
+                    </IndicatorSubtext>
+                </IndicatorContainer>
+            </StyledComponentsShadowDomProvider>
         );
     }
     else {
