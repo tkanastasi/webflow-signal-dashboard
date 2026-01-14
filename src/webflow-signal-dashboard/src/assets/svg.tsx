33d0b5f484834c4286c1c5f684a8a996b49edbcf
tkanastasi <159182171+tkanastasi@users.noreply.github.com>
2026-01-14 17:26:47 +0400

    Test 2

--- src/webflow-signal-dashboard/src/assets/svg.tsx
+++ src/webflow-signal-dashboard/src/assets/svg.tsx
@@ -7,7 +7,7 @@ export const TrendIcon: React.FC<IconProps> = (props) => (
         viewBox="0 0 24 24"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
-        stroke="#28af60"
+        stroke="currentColor"
         strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
